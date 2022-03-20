fa8154031d8ba33cde566eb766121d93515734fe
egormcobakaster <56125666+egormcobakaster@users.noreply.github.com>
2022-03-20 19:53:13 +0300

    Update train.py

--- projects/1/train.py
+++ projects/1/train.py
@@ -1,16 +1,12 @@
-#!/opt/conda/envs/dsenv/bin/python
-
-import os, sys
 import logging
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from joblib import dump
-
+import joblib
 #
 # Import model definition
 #
-from model import model, fields
+from model import model, fields, numeric_features, categorical_features, features
 
 
 #
@@ -25,7 +21,7 @@ logging.info("ARGS {}".format(sys.argv[1:]))
 # Read script arguments
 #
 try:
-  proj_id = sys.argv[1] 
+  proj_id = sys.argv[1]
   train_path = sys.argv[2]
 except:
   logging.critical("Need to pass both project_id and train dataset path")
@@ -41,22 +37,26 @@ logging.info(f"TRAIN_PATH {train_path}")
 #fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
 #num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
 
-read_table_opts = dict(sep=",", names=fields, index_col=False)
+read_table_opts = dict(sep="\t", names=fields, index_col=False)
 df = pd.read_table(train_path, **read_table_opts)
-
+ind = list(range(2, len(fields)))
 #split train/test
 X_train, X_test, y_train, y_test = train_test_split(
-    df.iloc[:,:-1], df.iloc[:,-1], test_size=0.33, random_state=42
+        df[numeric_features + categorical_features], df.iloc[:,1], test_size=0.33, random_state=42
 )
-
+print(numeric_features, categorical_features)
 #
 # Train the model
 #
-model.fit(X_train, y_train)
+model1 =model.fit(X_train, y_train)
 
-model_score = model.score(X_test, y_test)
+model_score = model1.score(X_test, y_test)
 
 logging.info(f"model score: {model_score:.3f}")
 
 # save the model
-dump(model, "{}.joblib".format(proj_id))
+joblib.dump(model1, "{}.joblib".format(proj_id))
+joblib_model = joblib.load("1.joblib")
+model_score = joblib_model.score(X_test, y_test)
+
+logging.info(f"model score: {model_score:.3f}")
