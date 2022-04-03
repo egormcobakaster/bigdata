5bc609dcd855db1d6f56c667c615c084ce3af69b
egormcobakaster <56125666+egormcobakaster@users.noreply.github.com>
2022-04-04 01:16:43 +0300

    Update shortest_path.py

--- projects/3/shortest_path.py
+++ projects/3/shortest_path.py
@@ -11,15 +11,30 @@ PYSPARK_HOME = os.path.join(SPARK_HOME, "python/lib")
 sys.path.insert(0, os.path.join(PYSPARK_HOME, "py4j-0.10.9.3-src.zip"))
 sys.path.insert(0, os.path.join(PYSPARK_HOME, "pyspark.zip"))
 
-from pyspark import SparkContext, SparkConf
- 
+import os
+import sys
+
+SPARK_HOME = "/usr/hdp/current/spark2-client"
+PYSPARK_PYTHON = "/opt/conda/envs/dsenv/bin/python"
+
+os.environ["PYSPARK_PYTHON"]= PYSPARK_PYTHON
+os.environ["SPARK_HOME"] = SPARK_HOME
+os.environ["PYSPARK_DRIVER_PYTHON"]= "/opt/conda/envs/dsenv/bin/python"
+os.environ["PYSPARK_EXECUTOR_PYTHON"]= "/opt/conda/envs/dsenv/bin/python"
+PYSPARK_HOME = os.path.join(SPARK_HOME, "python/lib")
+sys.path.insert(0, os.path.join(PYSPARK_HOME, "py4j-0.10.9.3-src.zip"))
+sys.path.insert(0, os.path.join(PYSPARK_HOME, "pyspark.zip"))
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
 conf = SparkConf()
-sc = SparkContext(appName="Pagerank", conf=conf)
+
+spark = SparkSession.builder.config(conf=conf).appName("Spark SQL").getOrCreate()
 
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
-import numpy as np
-import pandas as pd
+
 
 v_from = sys.argv[1]
 v_to = sys.argv[2]
@@ -49,13 +64,13 @@ while True:
 
     candidates = candidates.drop('v')
     candidates = candidates.withColumnRenamed('distance','dist')
-    candidates = candidates.withColumnRenamed('to_v','v1')
+    candidates = candidates.withColumnRenamed('to','v1')
     candidates = candidates.withColumnRenamed('from1','{}'.format(n))
     candidates = candidates.withColumn('dist', col('dist') + lit(1))
-
-
+    candidates.show()
+    print('{}'.format(n))
     new_distances = (distances
-                     .join(candidates, distances.v==candidates.v, how="full_outer")
+                     .join(candidates, distances.v==candidates.v1, how="full_outer")
                      .select("v1",
                              when(
                                  distances.distance.isNotNull(), distances.distance
@@ -71,6 +86,7 @@ while True:
         distances = candidates
         distances = distances.withColumnRenamed('v1','v')
         distances = distances.withColumnRenamed('dist','distance')
+        distances.show()
     else:
         break  
 
@@ -83,13 +99,9 @@ while True:
     if n>1000:
         break
 if n<1000:
-    distances.where(distances.v == v_to)
-    for row in Best.collect():
-        mas = row
-        break
-    mas1 = list(mas[1:len(mas)])
-    best_way = np.array(mas1[0:len(mas1)-2])
-    print(best_way)
-    best_way = np.append(best_way,(mas1[len(mas1)-2]), (mas1[len(mas1)-1]))
-    df = pd.DataFrame(best_way.reshape(1,-1))
-    df.to_csv(path, sep=',', index=False, header=False)
+    distances = distances.withColumn("tmp", distances.v)
+    distances = distances.withColumn("v", distances['{}'.format(n-1)])
+    distances = distances.withColumn('{}'.format(n-1), distances.tmp)
+    distances = distances.drop("tmp")
+    distances = distances.where(distances['{}'.format(n-1)]==v_to)
+    distances.write.format("csv").save(path, sep=',')
