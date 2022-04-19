174dddea6ae828501fe8ef0aa59026c119075eb3
egormcobakaster <56125666+egormcobakaster@users.noreply.github.com>
2022-04-19 23:24:16 +0300

    Update filter_cond.py

--- projects/1/filter_cond.py
+++ projects/1/filter_cond.py
@@ -6,6 +6,6 @@ def filter_cond(line_dict):
     Returns True if conditions are satisfied
     """
     cond_match = (
-       int(float(line_dict["if1"])) > 20 and int(float(line_dict["if1"])) < 40
+       line_dict['if1'] != '' and int(float(line_dict["if1"])) > 20 and int(float(line_dict["if1"])) < 40
     )
     return True if cond_match else False
