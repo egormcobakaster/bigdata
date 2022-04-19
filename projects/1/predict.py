7b0a560fd27db22b9ae79445bb113362a7ba91e5
egormcobakaster <56125666+egormcobakaster@users.noreply.github.com>
2022-04-19 23:39:16 +0300

    Update predict.py

--- projects/1/predict.py
+++ projects/1/predict.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 
 sys.path.append('.')
-from model import fields, numeric_features, categorical_features, features
+from model import fields
 
 #
 # Init the logger
