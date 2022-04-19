1029b78d8fdd875080a1f139dd170d09503b0970
egormcobakaster <56125666+egormcobakaster@users.noreply.github.com>
2022-04-19 23:17:09 +0300

    Update model.py

--- projects/1/model.py
+++ projects/1/model.py
@@ -2,7 +2,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 #
@@ -13,10 +13,11 @@ numeric_features = ["if"+str(i) for i in range(1,14)]
 categorical_features = ["cf"+str(i) for i in range(1,27)]+ ["day_number"]
 
 fields = ["id", "label"] + numeric_features + categorical_features
-features = ["id"] + numeric_features + categorical_features
-numeric_features = ["if"+str(i) for i in range(1,6)]+["if"+str(i) for i in range(12,14)]
-
-categorical_features = ["cf"+str(i) for i in range(1,7)]+["cf"+str(i) for i in range(20,27)]
+remove_cat_features = ['cf20', 'cf10', 'cf1', 'cf22', 'cf11', 'cf12', 'cf21', 'cf23', 'day_number']
+categorical_features_new = list(categorical_features)
+for feat in remove_cat_features:
+    categorical_features_new.remove(feat)
+new_fields = ["id", "label"] + numeric_features + categorical_features_new
 #
 # Model pipeline
 #
@@ -37,12 +38,16 @@ categorical_transformer = Pipeline(steps=[
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', numeric_transformer, numeric_features),
-        ('cat', categorical_transformer, categorical_features)
+        ('cat', categorical_transformer, categorical_features_new)
     ]
+)
+logreg_opts = dict(
+    solver='lbfgs',
+    class_weight='balanced',
 )
 
 # Now we have a full prediction pipeline.
 model = Pipeline(steps=[
     ('preprocessor', preprocessor),
-    ('linearregression', LinearRegression())
+    ('logreg', LogisticRegression(**logreg_opts))
 ])
