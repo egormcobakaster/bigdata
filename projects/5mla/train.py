11845dd0f58a29a6a010234cdaa7740f6591dfce
egormcobakaster <56125666+egormcobakaster@users.noreply.github.com>
2022-04-24 13:42:54 +0300

    Update train.py

--- projects/5mla/train.py
+++ projects/5mla/train.py
@@ -8,7 +8,66 @@ import joblib
 #
 # Import model definition
 #
-from model import model, fields
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
+import mlflow
+import mlflow.sklearn
+#
+# Dataset fields
+#
+try:
+  param = sys.argv[2]
+  train_path = sys.argv[1]
+except:
+  logging.critical("Need to pass both project_id and train dataset path")
+  sys.exit(1)
+numeric_features = ["if"+str(i) for i in range(1,14)]
+categorical_features = ["cf"+str(i) for i in range(1,27)]+ ["day_number"]
+
+fields = ["id", "label"] + numeric_features + categorical_features
+remove_cat_features = ['cf20', 'cf10', 'cf1', 'cf22', 'cf11', 'cf12', 'cf21', 'cf23']
+categorical_features_new = list(categorical_features)
+for feat in remove_cat_features:
+    categorical_features_new.remove(feat)
+new_fields = ["id", "label"] + numeric_features + categorical_features_new
+#
+# Model pipeline
+#
+
+# We create the preprocessing pipelines for both numeric and categorical data.
+
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+#    ('scaler', StandardScaler())
+])
+
+
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features_new)
+    ]
+)
+logreg_opts = dict(
+  #'lbfgs'
+    solver=param,
+    class_weight='balanced',
+)
+
+# Now we have a full prediction pipeline.
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('logreg', LogisticRegression(**logreg_opts))
+])
 
 
 #
@@ -22,12 +81,7 @@ logging.info("ARGS {}".format(sys.argv[1:]))
 #
 # Read script arguments
 #
-try:
-  proj_id = sys.argv[1]
-  train_path = sys.argv[2]
-except:
-  logging.critical("Need to pass both project_id and train dataset path")
-  sys.exit(1)
+
 
 
 logging.info(f"TRAIN_ID {proj_id}")
@@ -57,9 +111,14 @@ model_score = model1.score(X_test, y_test)
 
 logging.info(f"model score: {model_score:.3f}")
 
-# save the model
-joblib.dump(model1, "{}.joblib".format(proj_id))
-joblib_model = joblib.load("1.joblib")
-model_score = joblib_model.score(X_test, y_test)
+model_score = model1.score(X_test, y_test)
 
 logging.info(f"model score: {model_score:.3f}")
+with mlflow.start_run():
+    estimator = model()
+    mlflow.log_params(estimator.get_params())
+    estimator.fit(X_train, y_train)
+    y_pred = estimator.predict_proba(X_test)[:,1]
+    log_loss = sklearn.metrics.log_loss(y_test, y_pred)
+    mlflow.log_metric("log_loss", log_loss)
+    mlflow.sklearn.log_model(estimator, artifact_path="models")
