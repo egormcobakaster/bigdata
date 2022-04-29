05dd429820ca6c09af0d39efefeaf771c8f0eb55
egormcobakaster <56125666+egormcobakaster@users.noreply.github.com>
2022-04-29 22:56:06 +0300

    Update train.py

--- projects/5mla/train.py
+++ projects/5mla/train.py
@@ -53,7 +53,7 @@ categorical_transformer = Pipeline(steps=[
 
 preprocessor = ColumnTransformer(
     transformers=[
-        ('num', numeric_transformer, numeric_features)
+        ('num', numeric_transformer, numeric_features),
         ('cat', categorical_transformer, categorical_features_new)
     ]
 )
